d1643fce31ea643a7f3ae7945d561f808b7bbcdb
tyler <tylercharawell@gmail.com>
2021-08-27 11:23:08 -0700

    added router to index.html/app.js and some functions regarding login and signup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,73 @@
-import './App.css';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import NavBar from './components/NavBar'
+import Signup from './components/Signup'
+import Login from './components/Login'
+import Home from './components/Home'
+import Logout from './components/Logout'
+import CreateBlock from './components/CreateBlock'
+import EditBlock from './components/EditBlock'
+import {useState, useEffect} from 'react'
+import {Switch, Route, useHistory} from 'react-router-dom'
 
-function App() {
+const App = () => {
+  
+  const history = useHistory()
+  const [currentUser, setCurrentUser] = useState(null)
+  const [errors, setErrors] = useState([])
+  // const [playlist, setPlaylist] = useState([])
+
+  const handleUserLoginAndSignup = (data) => {
+    data.errors ? setErrors(data.errors) : setUserAndBlocks(data)
+    if(!data.errors){
+      history.push('/home')
+      setErrors([])
+    }
+  }
+  
+  const setUser = (data) => {
+    setCurrentUser(data.user)
+  }
+  
+  const fetchUser = () => {
+    fetch('/me')
+    .then(res => res.json())
+    .then(data => setUser(data))
+  }
+
+    
+  useEffect(() => {
+    fetchUser()
+  }, [])
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar currentUser={currentUser} />
+      <h1>
+      { currentUser ? `Hello ${currentUser.username}!` : null}
+      </h1>
+      <Switch>
+        <Route exact path='/home'>
+          <Home />
+        </Route>
+        <Route path='/signup'>
+          <Signup  />  
+        </Route>
+        <Route path='/login'>
+          <Login errors={errors} handleUserLoginAndSignup={handleUserLoginAndSignup} />
+        </Route>
+        <Route path='/logout'>
+          <Logout setCurrentUser={setCurrentUser}/>
+        </Route>
+        <Route path='/createplaylist'>
+          <CreatePlaylist />
+        </Route>
+        <Route path='/editplaylist'>
+          <EditPlaylist />
+        </Route>
+
+      </Switch>
     </div>
   );
 }
